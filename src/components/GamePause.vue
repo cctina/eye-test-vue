1ca69a75ed0dbdcfcd6c997c0f0f3735ea2ac143
Yu-Chen Hsu <every2ever@gmail.com>
2017-10-20 02:01:31 -0700

    Set game components switching logic, 'Game', 'GamePause'

---
+++ src/components/GamePause.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="lightbox">
+    <div class="container">
+      <header class="header">
+        <h1>Pause</h1>  
+      </header>
+      <div class="row">
+        <div class="col">
+          <button @click="resumeGame" class="btn btn-success btn-lg">Continue!</button>
+          <button @click="restartGame" class="btn btn-danger btn-lg">restart</button>
+        </div>
+      </div>  
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    resumeGame() {
+      this.$emit('gameResumed');
+    },
+    restartGame() {
+      this.$emit('gameRestarted');
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.lightbox {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: #efefef;
+  z-index: 100;
+}
+</style>
--- src/pages/Game.vue
+++ src/pages/Game.vue
@@ -1,8 +1,37 @@
 <template>
-  <h1>Game Started!</h1>
+  <main>
+    <h1>Game Started!</h1>  
+    <button @click="pauseGame" class="btn btn-warning btn-lg">Pause</button>
+    
+    <app-game-pause v-if="isPaused" @gameResumed="resumeGame" @gameRestarted="restartGame"></app-game-pause>
+  </main>  
 </template>
 
 <script>
+import GamePause from '../components/GamePause.vue';
+
+export default {
+  data() {
+    return {
+      isPaused: false
+    };
+  },
+  components: {
+    appGamePause: GamePause
+  },
+  methods: {
+    pauseGame() {
+      this.isPaused = true;
+    },
+    resumeGame() {
+      this.isPaused = false;
+    },
+    restartGame() {
+      this.isPaused = false;
+      // clean data
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
